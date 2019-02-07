59f8aaa4978d23a4081601fc85286d1035e75f3f
Deric Walintukan <deric.walintukan@gmail.com>
2019-02-07 13:15:55 +0700

    Change to GEC coin (#7)
    Change to GEC coin

--- apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
+++ apps/block_scout_web/lib/block_scout_web/views/layout_view.ex
@@ -37,7 +37,7 @@ defmodule BlockScoutWeb.LayoutView do
   end
 
   def network_title do
-    Keyword.get(application_config(), :network) || "GHU"
+    Keyword.get(application_config(), :network) || "GEC"
   end
 
   defp application_config do
