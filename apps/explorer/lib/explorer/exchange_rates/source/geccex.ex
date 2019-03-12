b352be5bd677c571dbfacbb18a5eb4813c61d2fb
Deric Walintukan <deric.walintukan@gmail.com>
2019-03-12 11:48:21 +0700

    Finish format_data for geccex

--- apps/explorer/lib/explorer/exchange_rates/source/geccex.ex
+++ apps/explorer/lib/explorer/exchange_rates/source/geccex.ex
@@ -11,22 +11,23 @@ defmodule Explorer.ExchangeRates.Source.GECCEX do
 
   @impl Source
   def format_data(data) do
-    IO.puts "format_data"
-    IO.inspect(data)
-    for item <- decode_json(data), not is_nil(item["data"]) do
-      data_obj = item["data"]
+    json = decode_json(data)
+    if Map.has_key?(json, "data") do
+      data_obj = json["data"]
+      usd_value = Map.get(data_obj, "usd", 0)
 
-      %Token{
-        available_supply: to_decimal(0),
-        btc_value: to_decimal(0),
-        id: "GHU",
-        last_updated: DateTime.utc_now(),
-        market_cap_usd: to_decimal(0),
-        name: "GHU",
-        symbol: "GHU",
-        usd_value: to_decimal(data_obj["usd"]),
-        volume_24h_usd: to_decimal(0)
-      }
+      [%Token{
+          available_supply: to_decimal(0),
+          btc_value: to_decimal(0),
+          id: "ghu",
+          last_updated: DateTime.utc_now(),
+          market_cap_usd: to_decimal(0),
+          name: "GHU",
+          symbol: "GHU",
+          usd_value: to_decimal(usd_value),
+          volume_24h_usd: to_decimal(0)
+        }
+      ]
     end
   end
 
@@ -41,7 +42,7 @@ defmodule Explorer.ExchangeRates.Source.GECCEX do
   def body do
     # GHU: Hardcoded GEC token symbol since pulling from GECCEX API.
     Jason.encode!(%{
-      "currency": "GEC"
+      currency: "GEC"
     })
   end
 
