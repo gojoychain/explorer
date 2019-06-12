746b7b3760e57a6cf2893788a0fd0b214b4acf60
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-12 15:50:16 +0700

    Update favicon

--- apps/block_scout_web/lib/block_scout_web/endpoint.ex
+++ apps/block_scout_web/lib/block_scout_web/endpoint.ex
@@ -27,9 +27,8 @@ defmodule BlockScoutWeb.Endpoint do
       android-chrome-512x512.png
       apple-touch-icon.png
       browserconfig.xml
-      favicon.ico
-      favicon-16x16.png
-      favicon-32x32.png
+      favicon_16x16.png
+      favicon_32x32.png
       mstile-150x150.png
       safari-pinned-tab.svg
       site.manifest
