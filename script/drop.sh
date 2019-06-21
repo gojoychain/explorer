9570ebd05919e204d138a03b04795e5e2ae6aa5b
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-21 13:09:45 +0700

    Add local docker config and add local for scripts

--- script/drop.sh
+++ script/drop.sh
@@ -3,7 +3,7 @@
 
 CHAIN=$1
 DB_ACTION=$2
-MESSAGE="$ ./run.sh [mainnet/testnet]"
+MESSAGE="$ ./run.sh [mainnet/testnet/local]"
 
 # Check for chain var
 if [ -z "${CHAIN}" ]
@@ -13,31 +13,13 @@ then
     exit 2
 fi
 
-# Assign vars for correct chain
+# Import .env vars for correct chain
 if [ "${CHAIN}" = "mainnet" ]; then
-    export PORT=5000
-    export MIX_ENV=dev
-    export NETWORK=Gojoychain
-    export SUBNETWORK=Mainnet
-    export COIN=JOY
-    export ETHEREUM_JSONRPC_VARIANT=geth
-    export ETHEREUM_JSONRPC_HTTP_URL=https://api.gojoychain.com
-    export ETHEREUM_JSONRPC_WS_URL=wss://api.gojoychain.com/ws
-    export ETHEREUM_JSONRPC_TRACE_URL=https://api.gojoychain.com
-    export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/explorer_mainnet
-    export ECTO_USE_SSL=true
+    export $(cat ./docker/mainnet/.env | xargs)
 elif [ "${CHAIN}" = "testnet" ]; then
-    export PORT=4000
-    export MIX_ENV=dev
-    export NETWORK=Gojoychain
-    export SUBNETWORK=Testnet
-    export COIN=JOY
-    export ETHEREUM_JSONRPC_VARIANT=geth
-    export ETHEREUM_JSONRPC_HTTP_URL=https://testapi.gojoychain.com
-    export ETHEREUM_JSONRPC_WS_URL=wss://testapi.gojoychain.com/ws
-    export ETHEREUM_JSONRPC_TRACE_URL=https://testapi.gojoychain.com
-    export DATABASE_URL=postgresql://postgres:postgres@localhost:5432/explorer_testnet
-    export ECTO_USE_SSL=true
+    export $(cat ./docker/testnet/.env | xargs)
+elif [ "${CHAIN}" = "local" ]; then
+    export $(cat ./docker/local/.env | xargs)
 else
     echo "invalid chain"
     echo ${MESSAGE}
