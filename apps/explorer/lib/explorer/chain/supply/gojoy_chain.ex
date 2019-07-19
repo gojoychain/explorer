7c06361aa5eb78b17395cfbbd6efc31ad430c600
Deric Walintukan <deric.walintukan@gmail.com>
2019-07-19 21:49:38 +0700

    Add logic to fetch balance from master addr

--- apps/explorer/lib/explorer/chain/supply/gojoy_chain.ex
+++ apps/explorer/lib/explorer/chain/supply/gojoy_chain.ex
@@ -4,11 +4,28 @@ defmodule Explorer.Chain.Supply.GojoyChain do
   """
   use Explorer.Chain.Supply
 
+  import Explorer.Chain, only: [
+    string_to_address_hash: 1,
+    hashes_to_addresses: 1
+  ]
+
   @total_supply 1_000_000_000
+  @master_address "0xd0B9f1f7742429A0768f561aeaBEc4a752578167"
 
   @spec circulating :: non_neg_integer()
   def circulating do
-    @total_supply
+    with {:ok, address_hash} <- string_to_address_hash(@master_address) do
+      addresses = hashes_to_addresses([address_hash])
+      address = List.first(addresses)
+      IO.inspect address
+      if address.fetched_coin_balance && address.fetched_coin_balance.value do
+        @total_supply - address.fetched_coin_balance.value
+      else
+        0
+      end
+    else
+      _ -> 0
+    end
   end
 
   @spec total :: non_neg_integer()
