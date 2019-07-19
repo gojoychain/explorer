11ea5598704754c9e8ab2503f3992ca517164e02
Deric Walintukan <deric.walintukan@gmail.com>
2019-07-19 18:29:03 +0700

    Add spec types

--- apps/explorer/lib/explorer/chain/supply/gojoy_chain.ex
+++ apps/explorer/lib/explorer/chain/supply/gojoy_chain.ex
@@ -6,10 +6,12 @@ defmodule Explorer.Chain.Supply.GojoyChain do
 
   @total_supply 1_000_000_000
 
+  @spec circulating :: non_neg_integer()
   def circulating do
     @total_supply
   end
 
+  @spec total :: non_neg_integer()
   def total do
     @total_supply
   end
